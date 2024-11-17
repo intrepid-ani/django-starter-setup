c84b2d86c2b77504300bd4e0dbd105cfe918a8aa
Ani <intrepidani05@gmail.com>
2024-11-17 11:22:32 +0530

    Configured the ''core/veiws.py'

--- MyProject/core/views.py
+++ MyProject/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse # This is just for testing purposes, you can remove this line 
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, World!") # This is just for testing purposes, you can remove this line
